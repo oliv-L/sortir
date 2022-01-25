b57596a21bc6ad912ce01c79bd355baf6eeddeab
romainbernier <romain.bernier2021@campus-eni.fr>
2022-01-25 13:54:49 +0100

    ajout image (facultatif) pour bg, participant, changement du uniqueEntity en annotation. traduction anglais/francais twig

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -14,7 +14,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ParticipantRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email", "pseudo"}, message="There is already an account with this email")
  */
 class Participant implements UserInterface, PasswordAuthenticatedUserInterface
 {
