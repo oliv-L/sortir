9a2c910515534927a573efdbc4568d92f8200390
CCoretin <corentin.pasquette2021@campus-eni.fr>
2022-01-26 13:16:13 +0100

    Affichage de la listes des sorties.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Participant;
-use App\Form\ProfilType;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,20 +11,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/sortir/profil", name="sortir_profil")
-     */
-    public function modifierProfil(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $profil = new Participant();
-        $profilForm = $this->createForm(ProfilType::class, $profil);
-        $profilForm->handleRequest($request);
-        if($profilForm->isSubmitted() && $profilForm->isValid()){
-            $entityManager->persist($profil);
-            $entityManager->flush();
-        }
-        return $this->render('sortir/profil.html.twig', [
-            'profilForm' => $profilForm->createView()
-        ]);
-    }
+
+    
+  
+    
 }
